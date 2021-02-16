645d1a732b9e4713a8a7d07508b71a6f05c087fe
Jaime <jasmo2.com@gmail.com>
2021-02-16 18:53:23 -0500

    get players

--- server.go
+++ server.go
@@ -1,7 +1,25 @@
-package server
+package main
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+	"strings"
+)
 
 func PlayerServer(w http.ResponseWriter, r *http.Request) {
+	player := strings.TrimPrefix(r.URL.Path, "/players/")
 
+	fmt.Fprint(w, GetPlayersScore(player))
+}
+
+func GetPlayersScore(name string) string {
+	if name == "Pepper" {
+		return "20"
+	}
+
+	if name == "Floyd" {
+		return "10"
+	}
+
+	return ""
 }
