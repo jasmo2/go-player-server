6d7de057d6644ec0a685f12f682bb5289d02b0dd
Jaime <jasmo2.com@gmail.com>
2021-02-17 13:54:42 -0500

    application/json Refactor

--- server_test.go
+++ server_test.go
@@ -138,13 +138,16 @@ func TestLeague(t *testing.T) {
 
 		assertStatus(t, response.Code, http.StatusOK)
 		assertLeague(t, got, wantedLeague)
-
-		if response.Result().Header.Get("content-type") != "application/json" {
-			t.Errorf("response did not have content-type of application/json, got %v", response.Result().Header)
-
-		}
+		assertContentType(t, response, jsonContentType)
 
 	})
+}
+func assertContentType(t testing.TB, response *httptest.ResponseRecorder, want string) {
+	t.Helper()
+	if response.Result().Header.Get("content-type") != jsonContentType {
+		t.Errorf("response did not have content-type of application/json, got %v", response.Result().Header)
+	}
+
 }
 
 func getLeagueFromResponse(t testing.TB, body io.Reader) (league []Player) {
