4f9b6f997433b263d33deec5607daaaf5c5a9b4b
Jaime <jasmo2.com@gmail.com>
2021-02-17 14:16:33 -0500

    remove warning by comments

--- in_memory_player_store.go
+++ in_memory_player_store.go
@@ -1,29 +1,30 @@
 package main
 
+// NewInMemoryPlayerStore initialises an empty player store.
 func NewInMemoryPlayerStore() *InMemoryPlayerStore {
-	return &InMemoryPlayerStore{
-		map[string]int{},
-	}
+	return &InMemoryPlayerStore{map[string]int{}}
 }
 
+// InMemoryPlayerStore collects data about players in memory.
 type InMemoryPlayerStore struct {
 	store map[string]int
 }
 
-func (i *InMemoryPlayerStore) RecordWin(name string) {
-	i.store[name]++
-}
-
-func (i *InMemoryPlayerStore) GetPlayerScore(name string) int {
-	return i.store[name]
-}
-
-// in_memory_player_store.go
+// GetLeague returns a collection of Players.
 func (i *InMemoryPlayerStore) GetLeague() []Player {
 	var league []Player
-
 	for name, wins := range i.store {
 		league = append(league, Player{name, wins})
 	}
 	return league
+}
+
+// RecordWin will record a player's win.
+func (i *InMemoryPlayerStore) RecordWin(name string) {
+	i.store[name]++
+}
+
+// GetPlayerScore retrieves scores for a given player.
+func (i *InMemoryPlayerStore) GetPlayerScore(name string) int {
+	return i.store[name]
 }
