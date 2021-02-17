59efe70273b146e7cef2589c061ead17d8d78d5f
Jaime <jasmo2.com@gmail.com>
2021-02-17 14:08:38 -0500

    Display League table from memory

--- in_memory_player_store.go
+++ in_memory_player_store.go
@@ -18,6 +18,12 @@ func (i *InMemoryPlayerStore) GetPlayerScore(name string) int {
 	return i.store[name]
 }
 
+// in_memory_player_store.go
 func (i *InMemoryPlayerStore) GetLeague() []Player {
-	return nil
+	var league []Player
+
+	for name, wins := range i.store {
+		league = append(league, Player{name, wins})
+	}
+	return league
 }
