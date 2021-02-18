54f4ec21e1f71ed56fb8b4c9c360d24c9d519ef9
Jaime <jasmo2.com@gmail.com>
2021-02-18 11:48:43 -0500

    change folder structure

--- cmd/webserver/main.go
+++ cmd/webserver/main.go
@@ -5,6 +5,8 @@ import (
 	"log"
 	"net/http"
 	"os"
+
+	"github.com/jasmo2/go-player-server/poker"
 )
 
 const dbFileName = "game.db.json"
@@ -16,13 +18,13 @@ func main() {
 		log.Fatalf("problem opening %s %v", dbFileName, err)
 	}
 
-	store, err := NewFileSystemPlayerStore(db)
+	store, err := poker.NewFileSystemPlayerStore(db)
 
 	if err != nil {
 		log.Fatalf("problem creating file system player store, %v ", err)
 	}
 
-	server := NewPlayerServer(store)
+	server := poker.NewPlayerServer(store)
 
 	if err := http.ListenAndServe(":5000", server); err != nil {
 		log.Fatalf("could not listen on port 5000 %v", err)
