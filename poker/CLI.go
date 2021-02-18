93a7ee853ef4668dc3b961a89103a98a1f6838f8
Jaime <jasmo2.com@gmail.com>
2021-02-18 15:10:40 -0500

    BlindAlerter

--- poker/CLI.go
+++ poker/CLI.go
@@ -4,24 +4,33 @@ import (
 	"bufio"
 	"io"
 	"strings"
+	"time"
 )
 
 // CLI struct
 type CLI struct {
 	playerStore PlayerStore
 	in          *bufio.Scanner
+	alerter     BlindAlert
+}
+
+// BlindAlert interface
+type BlindAlert interface {
+	ScheduleAlertAt(duration time.Duration, amount int)
 }
 
 //NewCLI initialiser
-func NewCLI(store PlayerStore, in io.Reader) CLI {
+func NewCLI(store PlayerStore, in io.Reader, alerter BlindAlert) CLI {
 	return CLI{
 		playerStore: store,
 		in:          bufio.NewScanner(in),
+		alerter:     alerter,
 	}
 }
 
 // PlayPoker func
 func (cli *CLI) PlayPoker() {
+	cli.alerter.ScheduleAlertAt(5*time.Second, 100)
 	input := cli.readLine()
 	cli.playerStore.RecordWin(extractWinner(input))
 }
