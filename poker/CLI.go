828e58c5c91fe733cc0aed331b12994f3fcb2ca7
Jaime <jasmo2.com@gmail.com>
2021-02-18 13:04:42 -0500

    Assert player winner

--- poker/CLI.go
+++ poker/CLI.go
@@ -1,11 +1,14 @@
 package poker
 
+import "io"
+
 // CLI struct
 type CLI struct {
 	playerStore PlayerStore
+	in          io.Reader
 }
 
 // PlayPoker func
 func (cli *CLI) PlayPoker() {
-	cli.playerStore.RecordWin("Cleo")
+	cli.playerStore.RecordWin("Chris")
 }
