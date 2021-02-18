eb7b408eb99184f6b89b05d1ba921a63db76fb33
Jaime <jasmo2.com@gmail.com>
2021-02-18 14:10:49 -0500

    FileSystem refactor to serve both applications

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -15,17 +15,12 @@ func main() {
 	fmt.Println("Let's play poker")
 	fmt.Println("Type {Name} wins to record a win")
 
-	db, err := os.OpenFile(dbName, os.O_RDWR|os.O_CREATE, 0666)
+	store, close, err := poker.FileSystemPlayerStoreFromFile(dbName)
 
 	if err != nil {
-		log.Fatalf("problem opening %s %v", dbName, err)
+		log.Fatal(err)
 	}
-
-	store, err := poker.NewFileSystemPlayerStore(db)
-
-	if err != nil {
-		log.Fatalf("problem creating file system player store, %v", err)
-	}
+	defer close()
 
 	game := poker.NewCLI(store, os.Stdin)
 	game.PlayPoker()
